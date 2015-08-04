c1e9bb2bcf8124fa0e6341864706b95eefce1d17
Yuri Klimov <yuri@klimov.net>
2015-08-04 13:45:57 +0300

    - Boot partition for Lime-A10. Boot partition size is 64MB.

--- configuration.sh
+++ configuration.sh
@@ -133,6 +133,7 @@ REVISION="1.0"
 BOOTCONFIG="A10-OLinuXino-Lime_defconfig"
 MODULES="hci_uart gpio_sunxi rfcomm hidp bonding spi_sun7i 8021q a20_tp"
 MODULES_NEXT=""
+BOOTSIZE="64"
 ;;
 
 
@@ -155,7 +156,7 @@ REVISION="1.9"
 BOOTCONFIG="A20-OLinuXino-Lime2_defconfig" 
 MODULES="hci_uart gpio_sunxi rfcomm hidp bonding spi_sun7i 8021q a20_tp"
 MODULES_NEXT=""
-BOOTSIZE="32"
+BOOTSIZE="64"
 ;;
 
 
